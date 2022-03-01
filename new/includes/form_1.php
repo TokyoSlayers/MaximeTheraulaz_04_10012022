2820c9f8b7e8435e80d1d6257c5e997ee01623e7
TokyoSlayer <theraulaz.max@hotmail.fr>
2022-03-01 13:46:06 +0100

    clean

--- new/includes/form_1.php
+++ new/includes/form_1.php
@@ -1,23 +1,30 @@
 <?php	
-	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	if(
+		(empty($_POST['NAME']) && strlen($_POST['NAME']) == 0) || 
+		(empty($_POST['EMAIL']) && strlen($_POST['EMAIL']) == 0) || 
+		(empty($_POST['QUESTION']) && strlen($_POST['QUESTION']) == 0) || 
+		(empty($_POST['CONTENT']) && strlen($_POST['CONTENT']) == 0)
+		)
 	{
 		return false;
 	}
+
 	
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$input_504 = $_POST['input_504'];
-	$message = $_POST['message'];
+	
+	$name = $_POST['NAME'];
+	$email = $_POST['EMAIL'];
+	$question = $_POST['QUESTION'];
+	$cotent = $_POST['CONTENT'];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Message from your website";
 	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+				  "Name: $name \nEmail: $email \nQuestion: $question \nMessage: $cotent \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
-	$headers .= "Reply-To: $input_504";	
+	$headers .= "Reply-To: $email";	
 	
 	mail($to,$email_subject,$email_body,$headers); // Post message
 	return true;			
